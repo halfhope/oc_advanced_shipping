b3e040ed1a0668c0ca4839963a855413ea1356f1
Talgat Shashakhmetov <talgatks@gmail.com>
2024-12-13 13:57:49 -0800

    Release v1.2

--- src/upload/admin/language/ru-ru/shipping/advanced_shipping.php
+++ src/upload/admin/language/ru-ru/shipping/advanced_shipping.php
@@ -14,6 +14,7 @@ $_['text_success']      = 'Настройки модуля обновлены!';
 $_['tab_common']   = 'Основные';
 $_['tab_price']    = 'Цена';
 $_['tab_weight']   = 'Вес';
+$_['tab_quantity'] = 'Кол-во товаров';
 $_['tab_customer'] = 'Группы пользователей';
 
 // Entry
@@ -22,26 +23,36 @@ $_['text_shipping']     = 'Доставка';
 $_['text_shippings']    = 'Варианты';
 $_['text_unregistered'] = 'Незарегистрированные';
 
-$_['entry_name']                = 'Название модуля';
+$_['entry_name']                = 'Название модуля:';
 $_['entry_cost']                = 'Стоимость доставки:';
 $_['entry_tax_class']           = 'Налоговый класс:';
 $_['entry_geo_zone']            = 'Географическая зона:';
 $_['entry_status']              = 'Статус:';
 $_['entry_sort_order']          = 'Порядок сортировки:';
+$_['entry_replace_free']        = 'Бесплатно при 0:';
+$_['entry_replace_free_help']   = 'При стоимости доставки 0, будет выводится "Бесплатно"';
 $_['entry_customer_group']      = 'Группы пользователей:';
 $_['entry_customer_group_help'] = 'Если оставить пустым, то группа пользователя не будет учитываться.';
 
 $_['entry_currency']     = 'Валюта:';
 $_['entry_weight_class'] = 'Весовой класс:';
 
-$_['entry_shipping_name']     = 'Название доставки:';
-$_['entry_price_range']       = 'Ценовой диапазон:';
-$_['entry_price_range_help']  = 'Если оставить пустым, то стоимость корзины не будет учитываться';
-$_['entry_weight_range']      = 'Весовой диапазон:';
-$_['entry_weight_range_help'] = 'Если оставить пустым, то вес корзины не будет учитываться';
-$_['entry_description']       = 'Описание доставки:';
+$_['entry_shipping_name'] = 'Название доставки:';
+$_['entry_description']   = 'Описание доставки:';
+
+$_['entry_price_range']             = 'Ценовой диапазон:';
+$_['entry_price_range_help']        = 'Если оставить пустым, то стоимость заказа не будет учитываться';
+$_['entry_price_range_description'] = '0, 300 - показывать при сумме заказа до 300 <br>100, 300 - показывать при сумме заказа от 100 до 300 <br> 300, 0 - показывать при сумме заказа от 300 и выше';
+
+$_['entry_weight_range']             = 'Весовой диапазон:';
+$_['entry_weight_range_help']        = 'Если оставить пустым, то вес корзины не будет учитываться';
+$_['entry_weight_range_description'] = '0, 300 - show with weight up to 300 <br> 100, 300 - show with weight from 100 to 300 <br> 300, 0 - show with weight from 300 and above';
+
+$_['entry_quantity_range']             = 'Количественный диапазон:';
+$_['entry_quantity_range_help']        = 'Если оставить пустым, то кол-во не будет учитываться';
+$_['entry_quantity_range_description'] = '0, 300 - показывать при кол-ве товаров в корзине до 300 <br>100, 300 - показывать кол-ве товаров от 100 до 300 <br> 300, 0 - показывать при кол-ве от 300 и выше';
 
 // Error
-$_['error_permission']      = 'У вас нет прав для редактирования настроек модуля!';
-$_['error_max_input_vars']  = 'Превышен лимит <a href="https://opencartforum.com/topic/41772-добавление-галочек-для-атреутов/?p=349655" target="_blank">max_input_vars</a>';
+$_['error_permission']     = 'У вас нет прав для редактирования настроек модуля!';
+$_['error_max_input_vars'] = 'Превышен лимит <a href="https://opencartforum.com/topic/41772-добавление-галочек-для-атреутов/?p=349655" target="_blank">max_input_vars</a>';
 ?>
